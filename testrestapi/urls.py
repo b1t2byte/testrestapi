ad4994401a6b996210602563b81f647e56965e93
Mironov Roman <vokabulyar@gmail.com>
2021-02-07 20:37:19 +0700

    Добавил SimpleRouter

--- testrestapi/urls.py
+++ testrestapi/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from products.views import ProductViewSet
+
+router = SimpleRouter()
+
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
