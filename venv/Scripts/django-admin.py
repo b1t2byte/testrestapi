ebe6bbe5a156dbdbf1648e6301d4cc7156687c1a
Mironov Roman <vokabulyar@gmail.com>
2021-02-10 15:52:49 +0700

    ДДобÐвил .gitignore

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\ROMAN\PycharmProjects\testrestapi\venv\Scripts\python.exe
+#!c:\users\roman\pycharmprojects\testrestapi\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
